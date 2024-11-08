60ab19267d45bcbe8b5b66bdb5500312fd5f5480
PhyTaffa <filippotaffarello@gmail.com>
2024-11-08 23:37:29 +0000

    Camera Zoom easing introduced
    use B to see it working

--- Unbox the Truth/Assets/Scripts/Player/CameraManager.cs
+++ Unbox the Truth/Assets/Scripts/Player/CameraManager.cs
@@ -1,4 +1,4 @@
-
+using System.Collections;
 using UnityEngine;
 
 namespace Player
@@ -6,33 +6,98 @@ namespace Player
     public class CameraManager : MonoBehaviour
     {
 
-        [SerializeField]private float minFOV = 67.0f;
+        [SerializeField] private float minFOV = 80.0f;
         [SerializeField] private float maxFOV = 120.0f;
+        private float currFOV = 80.0f;
+        [SerializeField] private float FOVChangeRate = 1f;
+        [SerializeField] private float cameraDepth = -10.0f;
         private GameObject playerGO;
         private Transform playerTransform;
+        
+        //buffer zone
+        private GameObject cameraBZ;
+        private Transform cameraBZTransform;
 
         public void Start()
         {
+            currFOV = minFOV;
+            
+            
             playerGO = GameObject.FindGameObjectWithTag("Player");
             playerTransform = playerGO.transform;
             
-            transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, -10);
+            
+            
+            transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, cameraDepth);
+            
+            //Camera Buffer zone
+            
+            //fetches the buffer zone component
+            cameraBZTransform = transform.GetChild(0);
+
+            if (cameraBZTransform != null)
+            {
+                Debug.Log("Child GameObject found: " + cameraBZTransform.name);
+            }
+            else
+            {
+                Debug.LogError("Child GameObject not found.");
+            }
+            
         }
         
         void Update () 
         {
-            transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, -10);
+            transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, cameraDepth);
+            MoveBufferZone(cameraBZTransform);
             //transform.position = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows 
-            //should be an easing
-            if (Input.GetKey(KeyCode.B))
+            
+            //uses the same input down. and decides which 
+            if (Input.GetKeyDown(KeyCode.B))
             {
-                //smooth with a loop, for now
-                Camera.main.fieldOfView = maxFOV;
+                StartCoroutine(PanCameraOut());
+                
+                //Camera.main.fieldOfView = maxFOV;
             }
-            else
+            
+            if(Input.GetKeyUp(KeyCode.B))
             {
-                Camera.main.fieldOfView = minFOV;
+                StartCoroutine(PanCameraIn());
+                
+                //Camera.main.fieldOfView = minFOV;
             }
+        }
+
+        private IEnumerator PanCameraOut()
+        {
+            while (currFOV < maxFOV)
+            { 
+                currFOV += FOVChangeRatePerFrame;
+                
+                Camera.main.fieldOfView = currFOV;
+
+                yield return null;
+                //yield return new WaitForSeconds(5); // Wait until the next frame
+            }
+
+            currFOV = maxFOV;
+        }
+        
+        private IEnumerator PanCameraIn()
+        {
+            while (currFOV > minFOV)
+            {
+                currFOV += -FOVChangeRatePerFrame;
+               
+                Camera.main.fieldOfView = currFOV;
+                yield return null; // Wait until the next frame
+            }
+            currFOV = minFOV;
+        }
+        
+        private void MoveBufferZone(Transform bufferZone)
+        {
+            bufferZone.position = new Vector3(transform.position.x, transform.position.y, -0.1f);
         }
     }
 }
