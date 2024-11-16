7d3bf1ae0529b039e1b827b236beaca4795975d8
PhyTaffa <filippotaffarello@gmail.com>
2024-11-16 20:59:50 +0000

    Door interaction fixed
    now the fish doens't block the interaction

--- Unbox the Truth/Assets/Scripts/Exit/Exit.cs
+++ Unbox the Truth/Assets/Scripts/Exit/Exit.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 using UnityEngine.Events;
 using UnityEngine;
 
-[System.Serializable] public class OnLevelExit: UnityEvent { }
+[System.Serializable]
+public class OnLevelExit : UnityEvent { }
 
 public class Exit : MonoBehaviour
 {
-    public OnLevelExit onLevelExitEvent;
+    [SerializeField] public OnLevelExit onLevelExitEvent;
+    [SerializeField] private LayerMask playerLayer;  // Specify the player layer
+
+    // private GameObject player;
+    // private Cheats cheats;
+
+    private void Start()
+    {
+        // player = GameObject.FindGameObjectWithTag("Player");
+        // cheats = player.GetComponent<Cheats>();
+    }
 
     private void OnTriggerStay2D(Collider2D col)
     {
-        if(Input.GetKeyDown(KeyCode.C))
+        // Check if the player is in the trigger and the correct key is pressed
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            if (col.gameObject.CompareTag("Player"))
+            // Check if the object entering the trigger is on the playerLayer
+            if (((1 << col.gameObject.layer) & playerLayer) != 0 && col.CompareTag("Player"))
             {
                 onLevelExitEvent?.Invoke();
                 Debug.Log("Exit");
             }
+            else if (((1 << col.gameObject.layer) & playerLayer) == 0)
+            {
+                // If it's not the player (e.g., the circle), ignore the interaction
+                Debug.Log("Ignoring non-player interaction.");
+            }
         }
-        
     }
-
 }
